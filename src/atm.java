125a3a82db0a60ba97916ef1550617094473bb18
bgrist18 <bgrist309@gmail.com>
2016-09-16 13:33:22 -0400

    updated version

--- src/atm.java
+++ src/atm.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 /**
@@ -19,9 +21,26 @@ public class atm {
 
 
             String answer = scanner.nextLine();
-            if (answer.equalsIgnoreCase("n")) {
+            if (answer.equalsIgnoreCase("no")) {
                 keepRunning = false;
             }
         }
     }
+    {ArrayList<String> names2 = new ArrayList();
+        names2.add("Alice");
+        names2.add("Bob");
+        names2.add("Charlie");
+        names2.remove(1);
+        System.out.println(names2);
+
+    HashMap<String, Object> person = new HashMap();
+        person.put("name", "Alice");
+        person.put("passord", "asdf");
+        person.put("name", "Bob");
+        person.put("password", "1234");
+        person.put("name", "Charlie");
+        person.put("password", "qwerty");
+
+        if (option.equalsIgnoreCase("balance")) {
+            System.out.println("Your balance is " + balance);
 }
