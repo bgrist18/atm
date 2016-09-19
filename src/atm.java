5aa2ba3aea1b50bd44c018f47792f415203ac237
bgrist18 <bgrist309@gmail.com>
2016-09-19 14:15:59 -0400

    updated version

--- src/atm.java
+++ src/atm.java
@@ -2,21 +2,26 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
 
-/**
- * Created by brettgrist on 9/14/16.
- */
 
 public class atm {
     static Scanner scanner = new Scanner(System.in);
 
+    static HashMap<String, Customer> data = new HashMap<>();
+
+
     public static void main(String[] args) throws Exception {
         System.out.println("Welcome ValuedCustomer");
+
+        data.put("Alice", new Customer("Alice", 100.00, "123"));
+        data.put("Bob", new Customer("Bob", 200.00, "asdf"));
+        data.put("Charlie", new Customer("Charlie", 300.00, "qwerty"));
 
         boolean keepRunning = true;
 
         while (keepRunning) {
             Customer customer = new Customer();
             customer.chooseName();
+            customer.addNewUser();
             customer.selectOption();
 
 
@@ -26,21 +31,4 @@ public class atm {
             }
         }
     }
-    {ArrayList<String> names2 = new ArrayList();
-        names2.add("Alice");
-        names2.add("Bob");
-        names2.add("Charlie");
-        names2.remove(1);
-        System.out.println(names2);
-
-    HashMap<String, Object> person = new HashMap();
-        person.put("name", "Alice");
-        person.put("passord", "asdf");
-        person.put("name", "Bob");
-        person.put("password", "1234");
-        person.put("name", "Charlie");
-        person.put("password", "qwerty");
-
-        if (option.equalsIgnoreCase("balance")) {
-            System.out.println("Your balance is " + balance);
 }
