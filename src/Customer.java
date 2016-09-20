7bb16609154dc1b23afd78a08515841e69644959
bgrist18 <bgrist309@gmail.com>
2016-09-19 21:11:57 -0400

    updated version

--- src/Customer.java
+++ src/Customer.java
@@ -1,39 +1,93 @@
-/**
- * Created by brettgrist on 9/14/16.
- */
 
 public class Customer {
     String name;
-    String balance;
-    String withdrawl;
-    String cancel;
-    String selectOption;
+    double balance;
+    //    String withdrawl;
+//    String cancel;
+//    String selectOption;
+    String password;
 
-    void chooseName(){
-        System.out.println("What is your name?");
-        name = atm.scanner.nextLine();
-        System.out.println("Welcome, " + name);
+
+
+    public Customer(String name, double balance, String withdrawl, String cancel, String selectOption, String password) {
+        this.name = name;
+        this.balance = balance;
+        this.password = password;
     }
-    void selectOption() throws Exception {
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Customer(String name, double balance, String password) {
+        this.name = name;
+        this.balance = balance;
+        this.password = password;
+    }
+
+    public Customer() {
+    }
+
+    void addPassword(Customer customer) {
+        System.out.println("Please enter a password");
+        String pass = atm.scanner.nextLine();
+        customer.password = pass;
+    }
+
+
+    public void selectOption() throws Exception {
         System.out.println("Check Balance, Withdrawl, Cancel");
         String option = atm.scanner.nextLine();
 
 
+
         if (option.equalsIgnoreCase("balance")) {
-            System.out.println("Your balance is $100");
+
+            System.out.println("Your balance is " + balance);
+
         } else if (option.equalsIgnoreCase("withdrawl")) {
             System.out.println("How much would you like to withdrawl");
             String withdrawl = atm.scanner.nextLine();
-            int withdrawlnum = Integer.valueOf(withdrawl);
-            if (withdrawlnum<100){
+            int withdrawlNum = Integer.valueOf(withdrawl);
+            if (withdrawlNum < 100) {
 
                 System.out.println("Please take your money");
-            }
-            else {
+
+                balance = (balance - withdrawlNum);
+                System.out.println("Your new balance is " + balance);
+                System.out.println("WouldYouLikeToMakeAnotherTransaction");
+                if (option.equalsIgnoreCase("yes")) {
+                    System.out.println("loop");
+                } else if (option.equalsIgnoreCase("no")) {
+                    System.out.println("Thank you, Have a great day!");
+                }
+//            Customer customer = new Customer();
+
+            } else {
                 System.out.println("Insufficient funds");
             }
 
-        } else if (option.equalsIgnoreCase("cancel")){
+        } else if (option.equalsIgnoreCase("cancel")) {
             System.out.println("Thank you, Have a great day!");
         }
 
